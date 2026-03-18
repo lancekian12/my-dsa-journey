b8322f916c572382db02729ce1f5be1383b87513
Lance Kian Flores <lancekian12@gmail.com>
2026-03-18 08:53:03 +0800

    different terms

---
+++ BIG(O)/Different-Terms.py
@@ -0,0 +1,10 @@
+def print_items(a , b):
+    for i in range(a):
+        print(i)
+    for j in range(b):
+        print(j)
+        
+def print_items(a , b):
+    for i in range(a):
+        for j in range(b):
+            print(i, j)
