d0f9094cf9419869cd7650d6a4c338dd35352189
Lance Kian Flores <lancekian12@gmail.com>
2026-03-26 09:33:22 +0800

    learn about pointers

---
+++ Classes & Pointers/pointers.py
@@ -0,0 +1,42 @@
+# num1 = 11
+
+# num2 = num1
+
+# print("Before num2 value is updated")
+# print("num1 = ", num1)
+# print("num2 = ", num2)
+
+# print("\nnum1 points to:", id(num1))
+# print("num2 points to:", id(num2))
+
+# num2 = 22
+
+# print("\nAfter num2 value is updated:")
+# print("num1 =", num1)
+# print("num2 =", num2)
+
+# print("\nnum1 points to:", id(num1))
+# print("num2 points to:", id(num2))
+
+####### Dictionary
+
+dict1 = {
+    'value': 11
+}
+dict2 = dict1
+
+print("\nBefore dict value is updated")
+print("dict1 = ", dict1)
+print("dict2 = ", dict2)
+
+print("\nAfter dict1 points to:", id(dict1))
+print("dict2 points to:", id(dict2))
+
+dict2['value'] = 22
+
+print("\nAfter value is updated:")
+print("dict1 = ", dict1)
+print("dict2 = ", dict2)
+
+print("\nAfter dict1 points to:", id(dict1))
+print("dict2 points to:", id(dict2))
